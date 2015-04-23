19ed7d873249c1f9af5401bb37c187126fea82da
Brennan Stehling <brennan@smallsharptools.com>
2015-04-22 23:25:33 -0700

    replaced http with Parse.Cloud.httpRequest

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ var distinctId = "abc123";
 // track an event related to a user with their distinctId
 
 mixpanel.track(eventName, properties).then(function() {
-	console.log("event was tracked");
+    console.log("event was tracked");
 }, function(error) {
-	console.log(error);
+    console.log(error);
 });
 
 // set properties for a user using their distinctId
 
-var properties = {"lastUseDate" : new Date()};
+var properties = {
+    "lastUseDate": new Date()
+};
 
 mixpanel.people.set(distinctId, properties).then(function(error) {
-	console.log("person properties were set");
+    console.log("person properties were set");
 }, function(error) {
-	console.log(error);
+    console.log(error);
 });
